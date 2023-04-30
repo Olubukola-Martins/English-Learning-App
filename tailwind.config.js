f122ab4bbd7ce6e0d206063e01762e2f6bec4d1f
Olubukola <19bukola97@gmail.com>
2023-04-30 13:43:05 +0100

    html and img files removed from source folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/index.html","./src/progresspage.html"],
+  content: ["./index.html","./progresspage.html"],
   theme: {
     extend: {},
   },
